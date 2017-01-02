e5fb93b2ca9fb11dad392cd51f0aa20f8478c8c2
Lloyd1nSpace <lloyd1nspace@icloud.com>
2017-01-02 17:30:24 -0500

    Couldnt get cells to move

--- CollectionViewCatPicPuzzle/CollectionViewController.swift
+++ CollectionViewCatPicPuzzle/CollectionViewController.swift
@@ -10,24 +10,98 @@ import UIKit
 
 class CollectionViewController: UICollectionViewController {
     
+    var headerReusableView: HeaderReusableView!
+    var footerReusableView: FooterReusableView!
+    var sectionInsets: UIEdgeInsets!
+    var spacing: CGFloat!
+    var itemSize: CGSize!
+    var referenceSize: CGSize!
+    var numberOfRows: CGFloat!
+    var numberOfColumns: CGFloat!
+    var imageSlices = [UIImage]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.collectionView?.register(HeaderReusableView.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "header")
+        self.collectionView?.register(FooterReusableView.self, forSupplementaryViewOfKind: UICollectionElementKindSectionFooter, withReuseIdentifier: "footer")
+        configureLayout()
+        
+        for image in 1...12 {
+            imageSlices.append(UIImage(named: "\(image)")!)
+        }
+    }
+    
+    func configureLayout() {
+        
+        let width = UIScreen.main.bounds.width
+        let height = UIScreen.main.bounds.height
+        
+        numberOfRows = 4
+        numberOfColumns = 3
+        spacing = 2
+        sectionInsets = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        referenceSize = CGSize(width: width, height: 60)
+        
+        let widthAdjustment = sectionInsets.left + sectionInsets.right + spacing * (numberOfColumns - 1)
+        let heightAdjustment = sectionInsets.top + sectionInsets.bottom + spacing * (numberOfRows - 1)
+        let itemWidth = (width - widthAdjustment) / numberOfColumns
+        let itemHeight = (height - heightAdjustment) / numberOfRows
+        
+        itemSize = CGSize(width: itemWidth, height: itemHeight)
         
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 12
+        return imageSlices.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
         let cell = self.collectionView?.dequeueReusableCell(withReuseIdentifier: "puzzleCell", for: indexPath) as! CollectionViewCell
-        
-        cell.imageView.image = UIImage(named: "cats")
-        
+        cell.imageView.image = imageSlices[Int(arc4random_uniform(UInt32(indexPath.row)))]
         return cell
-        
     }
     
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        if kind == UICollectionElementKindSectionHeader {
+            headerReusableView = (self.collectionView?.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "header", for: indexPath)) as! HeaderReusableView
+            return headerReusableView
+        } else {
+            footerReusableView = (self.collectionView?.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "footer", for: indexPath)) as! FooterReusableView
+            return footerReusableView
+        }
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, moveItemAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+
+    }
+    
+}
+
+extension CollectionViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return spacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return spacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return itemSize
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return referenceSize
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return referenceSize
+    }
     
 }
